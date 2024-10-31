6fc15e1b4cc41e8668032749184d92e228a9619e
Gregp-sys <P2879820@my365.dmu.ac.uk>
2024-10-31 17:59:14 +0000

    Delete Ecommerce1 directory

--- Ecommerce1/Views/CartItemViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿internal class CartItemViewModel
-{
-    public int Id_cart { get; set; }
-    public int Quantity { get; set; }
-    public string ProductName { get; set; }
-    public decimal Price { get; set; }
-}
